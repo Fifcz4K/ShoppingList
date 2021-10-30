1557bf59590f737e3c68d45db79334858df1edc2
Felipe <fifcz4k@gmail.com>
2021-10-31 00:57:57 +0200

    database window fully edited

--- ShoppingList/DishDatabaseWindow.xaml.cs
+++ ShoppingList/DishDatabaseWindow.xaml.cs
@@ -28,6 +28,7 @@ namespace ShoppingList
             dishList = new List<Dish>();
             ReadDatabase();
             WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            ingredientCategoryCombobox.ItemsSource = Enum.GetValues(typeof(IngredientCategory)).Cast<IngredientCategory>();
         }
 
         void ReadDatabase()
@@ -40,6 +41,9 @@ namespace ShoppingList
 
             if (dishList != null)
                 dishListView.ItemsSource = dishList;
+
+            if (dishListView.SelectedItem == null)
+                ingredientListView.ItemsSource = null;
         }
         void UpdateDatabase(Dish dish)
         {
@@ -51,11 +55,21 @@ namespace ShoppingList
         }
         private void dishListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            if (dishListView.SelectedItem != null)
+            if ((sender as ListView).SelectedItem != null)
             {
                 Dish dish = (dishListView.SelectedItem as Dish);
-                ingredientListView.ItemsSource = dish.GetIngredientList();
+                ingredientListView.ItemsSource = dish.GetIngredientList().OrderBy(x => x.Category);
                 dishNameTextbox.Text = dish.Name;
+            }
+        }
+
+        private void ingredientListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if ((sender as ListView).SelectedItem != null)
+            {
+                Ingredient ingredient = (ingredientListView.SelectedItem as Ingredient);
+                ingredientNameTextbox.Text = ingredient.Name;
+                ingredientCategoryCombobox.SelectedItem = ingredient.Category;
             }
         }
 
@@ -122,6 +136,9 @@ namespace ShoppingList
                 connection.Delete(dish);
             }
 
+            ingredientNameTextbox.Text = "";
+            ingredientCategoryCombobox.SelectedItem = null;
+
             ReadDatabase();
         }
 
@@ -131,23 +148,81 @@ namespace ShoppingList
             {
                 MessageBox.Show("You have to choose a dish", "Alert", MessageBoxButton.OK, MessageBoxImage.Error);
                 return;
-            }    
+            }
 
-            AddIngredientWindow addIngredientWindow = new AddIngredientWindow(dishListView.SelectedItem as Dish);
-            addIngredientWindow.ShowDialog();
+            if (string.IsNullOrEmpty(ingredientNameTextbox.Text) || !char.IsLetter(ingredientNameTextbox.Text[0]))
+            {
+                MessageBox.Show("Ingredient needs a valid name", "Alert", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            if (ingredientCategoryCombobox.SelectedItem == null)
+            {
+                MessageBox.Show("You have to choose a category of an ingredient", "Alert", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            Dish dish = (dishListView.SelectedItem as Dish);
+            dish.AddIngredient(new Ingredient(ingredientNameTextbox.Text, (IngredientCategory)ingredientCategoryCombobox.SelectedItem));
+
             UpdateDatabase(dishListView.SelectedItem as Dish);
-            ingredientListView.ItemsSource = (dishListView.SelectedItem as Dish).GetIngredientList();
+            ingredientListView.ItemsSource = (dishListView.SelectedItem as Dish).GetIngredientList().OrderBy(x => x.Category);
         }
 
-        private void ingredientListView_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        private void editIngredientButton_Click(object sender, RoutedEventArgs e)
         {
-            if (ingredientListView.SelectedItem == null)
+            if (dishListView.SelectedItem == null)
+            {
+                MessageBox.Show("You have to choose a dish", "Alert", MessageBoxButton.OK, MessageBoxImage.Error);
                 return;
+            }
 
-            ModifyIngredientWindow modifyIngredientWindow = new ModifyIngredientWindow(ingredientListView.SelectedItem as Ingredient, dishListView.SelectedItem as Dish);
-            modifyIngredientWindow.ShowDialog();
+            if (ingredientListView.SelectedItem == null)
+            {
+                MessageBox.Show("You have to choose an ingredient", "Alert", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            if (string.IsNullOrEmpty(ingredientNameTextbox.Text) || !char.IsLetter(ingredientNameTextbox.Text[0]))
+            {
+                MessageBox.Show("Ingredient needs a valid name", "Alert", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            if (ingredientCategoryCombobox.SelectedItem == null)
+            {
+                MessageBox.Show("You have to choose a category of an ingredient", "Alert", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            Dish dish = dishListView.SelectedItem as Dish;
+            Ingredient ingredient = ingredientListView.SelectedItem as Ingredient;
+            dish.UpdateIngredient(ingredient, new Ingredient(ingredientNameTextbox.Text, (IngredientCategory)ingredientCategoryCombobox.SelectedItem));
+
             UpdateDatabase(dishListView.SelectedItem as Dish);
-            ingredientListView.ItemsSource = (dishListView.SelectedItem as Dish).GetIngredientList();
+            ingredientListView.ItemsSource = (dishListView.SelectedItem as Dish).GetIngredientList().OrderBy(x => x.Category);
+        }
+
+        private void deleteIngredientButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (dishListView.SelectedItem == null)
+            {
+                MessageBox.Show("You have to choose a dish", "Alert", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            if (ingredientListView.SelectedItem == null)
+            {
+                MessageBox.Show("You have to choose an ingredient", "Alert", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            Dish dish = dishListView.SelectedItem as Dish;
+            Ingredient ingredient = ingredientListView.SelectedItem as Ingredient;
+            dish.RemoveIngredient(ingredient);
+
+            UpdateDatabase(dishListView.SelectedItem as Dish);
+            ingredientListView.ItemsSource = (dishListView.SelectedItem as Dish).GetIngredientList().OrderBy(x => x.Category);
         }
 
         private void closeDatabaseButton_Click(object sender, RoutedEventArgs e)
